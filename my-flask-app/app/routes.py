50316eb139d0a30b6affafebc20bc9686bc44f5b
PeterNg2333 <95013548+PeterNg2333@users.noreply.github.com>
2024-04-05 01:25:39 +0800

    Update routes.py

--- my-flask-app/app/routes.py
+++ my-flask-app/app/routes.py
@@ -1,6 +1,11 @@
 from app import app
+import time
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/time')
+def get_time():
+    return {"time": time.time()}
