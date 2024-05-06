8af5d949f5f3e054dd82009a4507b6b5516a578d
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-05-06 18:02:02 +0800

    Half done

--- my-flask-app/app/static/js/index.js
+++ my-flask-app/app/static/js/index.js
@@ -1,1 +1,16 @@
 // callback function after some time to hide the controls area
+// Upload the url to the server and display a loading spinner until the server responds
+function updateYTURL(event){
+    event.preventDefault();
+    // display a popup message and ask the user to provide a valid youtube url
+    var url = prompt("Please enter the youtube url", "https://www.youtube.com/watch?v=...");
+    if (url == null || url == "") {
+        alert("You must provide a valid youtube url");
+        return;
+    }
+    
+    // display a loading spinner
+    // Send the url to the server using fetch
+
+
+}
